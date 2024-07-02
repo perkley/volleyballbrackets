fc41e8aee02a8d041cec4d91de3f1cccee4d5313
Marvian-programmer <male.c201090@gmail.com>
2024-07-02 13:52:43 -0600

    style: Added more structure to the table.

--- amplify/southernlava-frontend/src/components/MainPage.jsx
+++ amplify/southernlava-frontend/src/components/MainPage.jsx
@@ -1,59 +1,57 @@
-import React, {useEffect, useState} from "react";
-import {getPoolPlaceData} from "../services/getPoolPlaceData";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const MainPage = () => {
-    const data = [
-        {teamId: 1, teamName: "Dragons", wins: 10, losses: 5, pointsDiff: 20, total: 15},
-        // ... other data objects
-    ]
-    const [teams, setTeams] = useState([])
-    teams.pointsDiff = undefined;
-    teams.place = undefined;
-    teams.wins = undefined;
-
-    useEffect(() => {
-        getPoolPlaceData().then((response) => {
-            setTeams(response.data);
-
-        }).catch((err) => {
-            console.error(err);
-        })
-
-    }, [])
-    return (
-        <div>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Team Id</TableCell>
-                            <TableCell>Team Name</TableCell>
-                            <TableCell>Wins</TableCell>
-                            <TableCell>Losses</TableCell>
-                            <TableCell>Point Differentials</TableCell>
-                            <TableCell>Total</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map((item) => (
-                            <TableRow key={item.teamId}>
-                                <TableCell>{item.teamId}</TableCell>
-                                <TableCell>{item.teamName}</TableCell>
-                                <TableCell>{item.wins}</TableCell>
-                                <TableCell>{item.losses}</TableCell>
-                                <TableCell>{item.pointsDiff}</TableCell>
-                                <TableCell>{item.total}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-
-
-        </div>
-
-
-    )
+function createData(teams, teamName, wins, losses, pointDifferential, total, place) {
+    return {teams, teamName, wins, losses, pointDifferential, total, place };
 }
-export default MainPage;
+
+const rows = [
+    createData('Team 1','ZTV 18 Meyer', 'IIII', 'II', '7 12 11 16 -16 -5', 25, 2),
+    createData('Team 2','Bonneville', 'IIIIII', '', '12 10 8 18 16 5', 69, 1),
+    createData('Team 3','Show Muzzy', 'II', 'IIII', '-7 -12 -8 -18 -10 12', -23, 3),
+    createData('Team 4','Elite Lynns', '', 'IIIIII', '-12 -10 -11 -16 -10 -12', -71, 4),
+];
+
+export default function BasicTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="Southern LA VolleyBall Association">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">Teams</TableCell>
+                        <TableCell align="right">Team Name</TableCell>
+                        <TableCell align="right">Wins</TableCell>
+                        <TableCell align="right">Losses</TableCell>
+                        <TableCell align="right">Point Differential</TableCell>
+                        <TableCell align="right">Total</TableCell>
+                        <TableCell align="right">Place</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.teams}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.teams}
+                            </TableCell>
+                            <TableCell align="right">{row.teamName}</TableCell>
+                            <TableCell align="right">{row.wins}</TableCell>
+                            <TableCell align="right">{row.losses}</TableCell>
+                            <TableCell align="right">{row.pointDifferential}</TableCell>
+                            <TableCell align="right">{row.total}</TableCell>
+                            <TableCell align="right">{row.place}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
